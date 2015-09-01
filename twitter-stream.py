73a255ab8a377ed8bf1435d094f6bf558d3c666b
Maxwell <maxwell.novais@titansgroup.com.br>
2015-09-01 17:10:05 -0300

    Refactor for using objects

--- twitter-stream.py
+++ twitter-stream.py
@@ -9,73 +9,90 @@ from elasticsearch import Elasticsearch
 from config import ELASTICSEARCH, TWITTER_API
 
 
-print('TWEPY - Twitter to Elasticsearch Interface with Python')
+def auth():
+    auth = OAuth(
+                 TWITTER_API["access_key"],
+                 TWITTER_API["access_secret"],
+                 TWITTER_API["consumer_key"],
+                 TWITTER_API["consumer_secret"]
+                 )
+    return auth
 
-auth = OAuth(TWITTER_API["access_key"], TWITTER_API["access_secret"],
-             TWITTER_API["consumer_key"], TWITTER_API["consumer_secret"])
 
-stream = TwitterStream(auth=auth)
-tweet_iter = stream.statuses.sample()
-# pattern = re.compile("%s" % stream_term, re.IGNORECASE)
+def return_datetime(string):
+    format = '%a %b %d %H:%M:%S %z %Y'
+    return datetime.strptime(string, format)
 
-es = Elasticsearch([{'host': ELASTICSEARCH['host'], 'port': int(ELASTICSEARCH['port'])}])
-session = requests.Session()
 
-for tweet in tweet_iter:
-    if 'delete' in tweet.keys():
-        pass
+def return_hashtags(tweet):
+    hashtags = []
+    if 'hashtags' in tweet['entities']:
+        for hashtag in tweet['entities']['hashtags']:
+            hashtags.append(hashtag['text'])
+    return hashtags
 
+
+def return_user_mentions(tweet):
+    user_mentions = []
+    if 'user_mentions' in tweet['entities']:
+        for users in tweet['entities']['user_mentions']:
+            user_mentions.append(users['screen_name'])
+    return user_mentions
+
+
+def contains_in_list(list):
+    if len(list) == 0:
+        return False
     else:
-        format = '%a %b %d %H:%M:%S %z %Y'
-        # Tue Sep 01 17:30:12 +0000 2015
-        timestamp = datetime.strptime(tweet['created_at'], format)
+        return True
 
-        hashtags = []
-        if 'hashtags' in tweet['entities']:
-            use_hashtag = True
-            for hashtag in tweet['entities']['hashtags']:
-                hashtags.append(hashtag['text'])
 
-        if len(hashtags) == 0:
-            hashtags = None
-            use_hashtag = False
-
-        user_mentions = []
-        if 'user_mentions' in tweet['entities']:
-            use_mention = True
-            for users in tweet['entities']['user_mentions']:
-                user_mentions.append(users['screen_name'])
-
-        if len(user_mentions) == 0:
-            user_mentions = None
-            use_mention = False
-
-        result = {
-            'lang': tweet['lang'],
-            'text': tweet['text'],
-            'retweeted': tweet['retweeted'],
-            'retweet_count': tweet['retweet_count'],
-            '@timestamp': timestamp,
-            'user': tweet['user']['screen_name'],
-            'user_friends': tweet['user']['friends_count'],
-            'user_lang': tweet['user']['lang'],
-            'user_geo_enable': tweet['user']['geo_enabled'],
-            'coordinates': tweet['coordinates'],
-            'geo': tweet['geo'],
-            'hashtags': hashtags,
-            'user_mentions': user_mentions,
-            'use_hashtag': use_hashtag,
-            'use_mentions': use_mention,
-            }
-
+def send_to_es(body):
+    es = Elasticsearch([{'host': ELASTICSEARCH['host'], 'port': int(ELASTICSEARCH['port'])}])
+    session = requests.Session()
+    r = session.get('http://{}:{}'.format(ELASTICSEARCH['host'], ELASTICSEARCH['port']))
+    if r.status_code == 200:
+        es.create(index=ELASTICSEARCH['index'], doc_type=ELASTICSEARCH['doc_type'], id=uuid.uuid4().hex, body=body)
         sys.stdout.write('.')
-        sys.stdout.flush()
+    else:
+        sys.stdout.write('E')
+    sys.stdout.flush()
 
-        try:
-            r = session.get('http://{}:{}'.format(ELASTICSEARCH['host'], ELASTICSEARCH['port']))
-            if r.status_code == 200:
-                es.create(index=ELASTICSEARCH['index'], doc_type=ELASTICSEARCH['doc_type'], id=uuid.uuid4().hex, body=result)
-            else:
-                raise Exception
-        except Exception as exc:
-            print('Error: {}'.format(exc))
+
+if __name__ == '__main__':
+    print('TWEPY - Twitter to Elasticsearch Interface with Python')
+    stream = TwitterStream(auth=auth())
+    tweet_iter = stream.statuses.sample()
+
+    for tweet in tweet_iter:
+        if 'delete' in tweet.keys():
+            pass
+
+        else:
+            timestamp = return_datetime(tweet['created_at'])
+
+            hashtags = return_hashtags(tweet)
+            use_hashtags = contains_in_list(hashtags)
+
+            user_mentions = return_user_mentions(tweet)
+            use_mentions = contains_in_list(user_mentions)
+
+            result = {
+                'lang': tweet['lang'],
+                'text': tweet['text'],
+                'retweeted': tweet['retweeted'],
+                'retweet_count': tweet['retweet_count'],
+                '@timestamp': timestamp,
+                'user': tweet['user']['screen_name'],
+                'user_friends': tweet['user']['friends_count'],
+                'user_lang': tweet['user']['lang'],
+                'user_geo_enable': tweet['user']['geo_enabled'],
+                'coordinates': tweet['coordinates'],
+                'geo': tweet['geo'],
+                'hashtags': hashtags,
+                'user_mentions': user_mentions,
+                'use_hashtags': use_hashtags,
+                'use_mentions': use_mentions,
+                }
+
+            send_to_es(result)
